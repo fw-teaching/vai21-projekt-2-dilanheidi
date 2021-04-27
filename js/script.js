fe14831244052150e446272cbf157f584a7c11f5
saarishe <70575886+saarishe@users.noreply.github.com>
2021-04-27 12:57:40 +0300

    get data med jQuery (måst städa data (?))

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ console.log('load');
 
 //Get data from API for 30 days (1.3-30.1)
 //[time, low, high, open, close, volume]
-
-//DEHÄR ÄR SÄKERT INT RÄTT....
-var data = d3.json("https://api.pro.coinbase.com/products/BTC-EUR/candles?start=2021-03-01&end=2021-03-30&granularity=86400");
-
-console.log(data);
+$.getJSON('https://api.pro.coinbase.com/products/BTC-EUR/candles?start=2021-03-01&end=2021-03-30&granularity=86400', function(data) {
+    console.log("Hela dataset: " + data);
+    console.log("Första: " + data[0]);
+    //måst kanske städa data, so it makes sense
+});
