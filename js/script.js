e9f23e1930cb2ae2c9088f5ef475a06d164ac770
saarishe <70575886+saarishe@users.noreply.github.com>
2021-04-28 12:21:03 +0300

    get data when clicking a button

--- js/script.js
+++ js/script.js
@@ -12,13 +12,15 @@ startdate = startdate.getFullYear() + '-' + (startdate.getMonth() + 1) + '-' + s
 console.log("today:" + enddate);
 console.log("30 days ago: " + startdate);
 
-var url = 'https://api.pro.coinbase.com/products/BTC-EUR/candles?start=' + startdate + '&end=' + enddate + '&granularity=86400'
+//var url = 'https://api.pro.coinbase.com/products/BTC-EUR/candles?start=' + startdate + '&end=' + enddate + '&granularity=86400'
 
-console.log(url);
-//Get data from API for 30 days (1.3-30.1)
-//[time, low, high, open, close, volume]
-$.getJSON(url, function(data) {
-    console.log("Hela dataset: " + data);
-    console.log("Första: " + data[0]);
-    //måst kanske städa data, so it makes sense
-});
+function loadData() {
+    var url = 'https://api.pro.coinbase.com/products/BTC-EUR/candles?start=' + startdate + '&end=' + enddate + '&granularity=86400'
+        //Get data from API for 30 days 
+        //[time, low, high, open, close, volume]
+    $.getJSON(url, function(data) {
+        console.log("Hela dataset: " + data);
+        console.log("Första: " + data[0]);
+        //måst kanske städa data, so it makes sense
+    });
+}
